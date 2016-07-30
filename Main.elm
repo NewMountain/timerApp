7074868ec215efe192bfa44afd00bce1ff4e0c1a
Christopher Nyberg <christopher.nyberg@gmail.com>
2016-07-30 00:05:48 -0700

    Updated subscriptions and update
    Updated the subscription and update functions. Core functionality is
    completed. Now we need to add some UI and a few buttons as well as some
    CSS. Looking solid though.

--- Main.elm
+++ Main.elm
@@ -1,6 +1,8 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (..)
+import Html.App as App
+import Time exposing (Time, second)
 
 
 -- Model
@@ -27,9 +29,124 @@ type alias Model =
 
 
 -- Update
+
+
+type Msg
+    = Tick Time
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        -- I don't care about the time, I just want the tick
+        Tick _ ->
+            case
+                ( model.counting
+                , model.timerStatus
+                , model.seconds
+                )
+            of
+                -- Not counting, so do nothing
+                ( False, _, _ ) ->
+                    ( model, Cmd.none )
+
+                -- Counting and the clock has struck 25 minutes in Focus
+                ( True, Focus, 1500 ) ->
+                    ( model
+                        |> flipStatus
+                        |> zeroClock
+                    , Cmd.none
+                    )
+
+                -- Counting and clock has struck 5 minutes in Relax
+                ( True, Relax, 300 ) ->
+                    ( model
+                        |> flipStatus
+                        |> zeroClock
+                        |> markPomComplete
+                    , Cmd.none
+                    )
+
+                -- Ordinary counting
+                ( True, _, s ) ->
+                    ( model
+                        |> tickSecond s
+                    , Cmd.none
+                    )
+
+
+tickSecond : Int -> Model -> Model
+tickSecond s model =
+    { model | seconds = s + 1 }
+
+
+flipStatus : Model -> Model
+flipStatus model =
+    case (model.timerStatus) of
+        Focus ->
+            { model | timerStatus = Relax }
+
+        Relax ->
+            { model | timerStatus = Focus }
+
+
+zeroClock : Model -> Model
+zeroClock model =
+    { model | seconds = 0 }
+
+
+markPomComplete : Model -> Model
+markPomComplete model =
+    { model | pomsCompleted = model.pomsCompleted + 1 }
+
+
+
 -- View
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ text "Ahoy world!"
+        , p [] [ text <| toString model ]
+        ]
+
+
+
 -- Init
 
 
+init : ( Model, Cmd Msg )
+init =
+    ( { counting = False
+      , timerStatus = Relax
+      , timerMode = Elapsed
+      , seconds = 0
+      , pomsCompleted = 0
+      }
+    , Cmd.none
+    )
+
+
+
+-- Subscription
+-- Trust me on this one (we can make MUVIS instead!!!)
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Time.every second Tick
+
+
+
+-- Main
+
+
+main : Program Never
 main =
-    text "Ahoy world!"
+    App.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
